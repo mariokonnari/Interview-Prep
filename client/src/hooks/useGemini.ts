9eabe2f77403c9d62e9f11b4364789c04638aff2
Marios Konnaris <mariokonnari@gmail.com>
2026-04-08 21:56:49 +0300

    Trying different AI model

--- client/src/hooks/useGemini.ts
+++ client/src/hooks/useGemini.ts
@@ -2,7 +2,7 @@ import { useState } from "react";
 import type { Feedback } from "../types";
 
 const GEMINI_API_URL =
-  'https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash-latest:generateContent'
+  'https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent'
 
 export function useGemini() {
     const [loading, setLoading] = useState(false)
