c317df2cb3ae7c9f5624f267a38491493c50c77f
Marios Konnaris <mariokonnari@gmail.com>
2026-04-09 14:25:28 +0300

    fix tsconfig rootDir and remove dist from git

--- server/dist/prisma.config.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const config_1 = require("prisma/config");
-exports.default = (0, config_1.defineConfig)({
-    datasource: {
-        url: "postgresql://postgres.xsovnixboqgmdqgmkkpb:ENQO4TELmwta1Mdr@aws-0-eu-west-1.pooler.supabase.com:5432/postgres?pgbouncer=true",
-    },
-});
