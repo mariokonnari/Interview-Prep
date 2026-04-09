36405515446aa4a8efa5abf777c6df66877a1b7a
Marios Konnaris <mariokonnari@gmail.com>
2026-04-09 13:57:44 +0300

    simplify prisma client - remove adapter pattern

--- server/prisma.config.ts
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'prisma/config'
-
-export default defineConfig({
-  datasource: {
-    url: "postgresql://postgres.xsovnixboqgmdqgmkkpb:ENQO4TELmwta1Mdr@aws-0-eu-west-1.pooler.supabase.com:5432/postgres?pgbouncer=true",
-  },
-})
--- server/src/lib/prisma.ts
+++ server/src/lib/prisma.ts
@@ -1,25 +1,5 @@
 import { PrismaClient } from '@prisma/client'
-import { PrismaPg } from '@prisma/adapter-pg'
-import { Pool } from 'pg'
 
-declare global {
-  var prisma: PrismaClient | undefined
-}
-
-function createPrismaClient() {
-  const connectionString = process.env.DATABASE_URL
-  if (!connectionString) {
-    throw new Error('DATABASE_URL environment variable is not set')
-  }
-  const pool = new Pool({ connectionString })
-  const adapter = new PrismaPg(pool)
-  return new PrismaClient({ adapter })
-}
-
-const prisma = global.prisma ?? createPrismaClient()
-
-if (process.env.NODE_ENV !== 'production') {
-  global.prisma = prisma
-}
+const prisma = new PrismaClient()
 
 export default prisma
