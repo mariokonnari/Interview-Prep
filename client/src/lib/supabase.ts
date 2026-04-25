aabc4a64a97be073da5806260e4aa7ab312d125e
Marios Konnaris <mariokonnari@gmail.com>
2026-04-25 10:01:31 +0300

    add: client/src/lib/supabase.ts

---
+++ client/src/lib/supabase.ts
@@ -0,0 +1,10 @@
+import { createClient } from "@supabase/supabase-js";
+
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
+const supabaseAnonkey = import.meta.env.VITE_SUPABASE_ANON_KEY
+
+if (!supabaseUrl || !supabaseAnonkey) {
+    throw new Error('Missing Supabase environment variables')
+}
+
+export const supabase = createClient(supabaseUrl, supabaseAnonkey)
